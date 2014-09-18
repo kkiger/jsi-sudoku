8cc0a08d5e03dc499aa34350be907553bc428370
Kali Kiger <kalikiger@gmail.com>
2014-09-18 13:59:00 -0700

    started test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var lib = require('../index');
+
+describe('scan numbers', function(){
+  it('should go through a list of numbers and returns if it does/does not exist', function(){
+    expect(result).to.eql.(true);
+  });
+
+  it('should go through a list of numbers and returns if it does/does not exist', function(){
+  expect(result).to.eql.(false);
+});
+
+});
