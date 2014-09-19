8153ce45801180c3c5419a9b0a629ee96f18bd97
nonogon <leslie.strauss@gmail.com>
2014-09-18 17:27:43 -0700

    wrote scanRowContents and scanRow function, started getColumnContents function

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 var Puzzle = function(puzzleString) {
   this._puzzle = puzzleString;
-
 };
+
+/**
+ * [getRowContents description]
+ * @param  {[type]} rowNumber [description]
+ * @return {String}           [description]
+ */
 Puzzle.prototype.getRowContents = function(rowNumber) {
-  return this._puzzle.substr((rowNumber*9),9);
-}
+  return this._puzzle.substr((rowNumber * 9),9);
+};
 
+/**
+ * Scan a row for a specific number.
+ *
+ * @param {Number} row - The row to scan.
+ * @param {Number} n - The number to scan for.
+ * @returns {Boolean} Whether or not the row contains the number.
+ */
 Puzzle.prototype.scanRow = function(row, n) {
+  var contents = this.getRowContents(row);
+  var index = contents.indexOf(n.toString());
+  return (index !== -1);
+};
 
-  var row = this.getRowContents(row);
-  //compare the contents form the called row to the `n`
+/**
+ * [getColumnContents description]
+ * @param  {[type]} columnNumber [description]
+ * @return {String}              [description]
+ */
+Puzzle.prototype.getColumnContents = function(columnNumber) {
+  var arraysAreBetter = [];
 
+  return arraysAreBetter.join('');
 
-
-  return row;
+  return this._puzzle.substr((columnNumber * 9), 9);
 };
 
 Puzzle.prototype.scanColumn = function(column, n) {
-  
+  var column = this.getColumnContents(column);
   return false;
 };
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect;
 var Puzzle = require('../index');
 
+
+
 describe('scan numbers', function(){
   it('check if there is a 1 in row 0', function(){
     // we have a 1
@@ -9,6 +11,13 @@ describe('scan numbers', function(){
     var row = 0;
     var result = puzzle.scanRow(row, 1);
     expect(result).to.eql(true);
+  });
+
+  it('check if there is a 3 in row 0', function(){
+    var puzzle = new Puzzle('158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413');
+    var row = 0;
+    var result = puzzle.scanRow(row, 3);
+    expect(result).to.eql(false);
   });
 
   it('check if there is a 5 in column 8', function(){
@@ -22,9 +31,21 @@ describe('scan numbers', function(){
     var puzzle = new Puzzle('158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413');
     expect(puzzle.getRowContents(0)).to.eql('158 2  6 ');
   });
+
    it('finds the contents of a specific row', function() {
     var puzzle = new Puzzle('158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413');
     expect(puzzle.getRowContents(1)).to.eql('2   8  9 ');
   });
 
+});
+
+describe('whit has an example', function() {
+  it('shows that example', function() {
+    var _ = require('lodash');
+    var string = '     h    e    l    l    o';
+    _.range(0, 30, 5).forEach(function(numberGeneratedByTheRange) {
+      var index = numberGeneratedByTheRange;
+      console.log(string[index]);
+    });
+  });
 });
