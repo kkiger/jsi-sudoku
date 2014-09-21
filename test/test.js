21b49191f4c3ccd15e94c29df4a635719522c849
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-20 20:18:43 -0700

    Update solver test.

--- test/test.js
+++ test/test.js
@@ -97,4 +97,13 @@ describe('scan numbers', function(){
   });
 });
 
+describe('solve puzzle', function(){
+  it('give missing numbers for a puzzle', function(){
+    // we have a 1
+    // we want to scan the row and see if there is another 1
+    var puzzle = new Puzzle('158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413');
+    var result = puzzle.solvePuzzle(;
+    expect(result).to.eql('158924367247683195561748239394562781782319654419837526623451978875296413');
+  });
+  
 
